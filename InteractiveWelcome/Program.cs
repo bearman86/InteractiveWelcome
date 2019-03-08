ee2706ed354d3dbe6122ea6cb5bd4a739d3fb38c
Tony Wieder <bearman86@live.com>
2019-03-07 16:38:39 -0800

    Shutdow code added

--- InteractiveWelcome/Program.cs
+++ InteractiveWelcome/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.DirectoryServices;
+using System.Diagnostics;
 
 
 namespace InteractiveWelcome
@@ -8,68 +9,80 @@ namespace InteractiveWelcome
     {
         static void Main(string[] args)
         {
-
-            while (true)
+            var doExit = false;
+            while (doExit != true)
             {
-                string myChoice;
+                string ldapFilter = "";
                 string ou = "";
-                do
+                Console.WriteLine("Choose the OU you want to Search\n");
+                Console.WriteLine("1 - Nellis");
+                Console.WriteLine("2 - Arcata Way");
+                Console.WriteLine("3 - Computer Name");
+                Console.WriteLine("S - Shutdown Computer");
+                Console.WriteLine("R - Reboot Computer");
+
+                Console.WriteLine("Q - Quit");
+
+                char myChoice = Console.ReadKey(true).KeyChar;
+
+                switch (myChoice)
                 {
-                    Console.WriteLine("Choose the OU you want to Search\n");
-                    Console.WriteLine("1 - Nellis");
-                    Console.WriteLine("2 - Arcata Way");
-                    Console.WriteLine("Q - Quit");
-
-                    myChoice = Console.ReadLine();
-
-                    switch (myChoice)
-                    {
-                        case "1":
-                            ou = "LDAP://OU=Arcata Way,OU=NTTR,OU=Workstations,DC=jt3,DC=com";
-                            break;
-                        case "2":
-                            ou = "LDAP://OU=Nellis,OU=NTTR,OU=Workstations,DC=jt3,DC=com";
-                            break;
-                        case "Q":
-                            Environment.Exit(0);
-                            break;
-                        case "q":
-                            Environment.Exit(0);
-                            break;
-                        default:
-                            Console.WriteLine("{0} is not a valid choice", myChoice);
-                            break;
-                    }
-                } while (myChoice != "1" && myChoice != "2");
-
-
-                Console.Write("Choose the number for the OU you want to search.\n");
+                    case '1':
+                        ou = "LDAP://OU=Arcata Way,OU=NTTR,OU=Workstations,DC=jt3,DC=com";
+                        ldapFilter = "(&(objectClass=computer))";
+                        break;
+                    case '2':
+                        ou = "LDAP://OU=Nellis,OU=NTTR,OU=Workstations,DC=jt3,DC=com";
+                        ldapFilter = "(&(objectClass=computer))";
+                        break;
+                    case '3':
+                        ldapFilter = "(&(objectClass=computer)(cn=LSV-VM-TRN*))";
+                        break;
+                    case 'S':
+                    case 's':
+                        Process.Start("shutdown", @"/s /m \\LSV-VM-TRN1 /t 0");
+                        Console.Clear();
+                        continue;
+                    case 'R':
+                    case 'r':
+                        Process.Start("shutdown", @"/r /m \\LSV-VM-TRN1 /t 0");
+                        Console.Clear();
+                        continue;
+                    case 'Q':
+                    case 'q':
+                        doExit = true;
+                        continue;
+                    default:
+                        Console.WriteLine("{0} is not a valid choice", myChoice);
+                        continue;
+                }
                 DirectoryEntry entry = new DirectoryEntry("LDAP://JT3-DC-ARC.JT3.com")
-                {
-                    Path = ou,
-                    AuthenticationType = AuthenticationTypes.Secure
-                };
+                    {
+                        Path = ou,
+                        AuthenticationType = AuthenticationTypes.Secure
+                    };
 
                 using (DirectorySearcher ds = new DirectorySearcher(entry))
                 {
                     ds.PropertiesToLoad.Add("name");
-                    ds.Filter = "(&(objectClass=computer))";
+                    ds.Filter = ldapFilter;
                     ds.Sort.Direction = SortDirection.Ascending;
                     ds.Sort.PropertyName = "name";
                     SearchResultCollection results = ds.FindAll();
 
                     int itemCount = results.Count;
-
+                 
                     foreach (SearchResult result in results)
                     {
-                        Console.WriteLine("{0}", result.Properties["name"][0].ToString());
+
+                        Console.WriteLine("{0}" , result.Properties["name"][0].ToString());
                     }
                     Console.WriteLine("Computer Count: {0}", itemCount);
-                    Console.Read();
+                    Console.ReadKey();
                     Console.Clear();
                 }
             }
-            
+            Environment.Exit(0);
         }
     }
     
